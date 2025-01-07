21d64d9a090dab0171f870d2db8938375b6903bd
snehith1438 <snehithgoudmbnr@gmail.com>
2025-01-07 21:54:16 +0530

    Add files via upload

---
+++ 2311cs020629(day-1).py
@@ -0,0 +1,11 @@
+# Declaring variables of different data types
+integer_var = 42           # Integer
+float_var = 3.14           # Float
+string_var = "Hello, world!"  # String
+boolean_var = True         # Boolean
+
+# Printing each variable and its type
+print("Integer Variable:", integer_var, "Type:", type(integer_var))
+print("Float Variable:", float_var, "Type:", type(float_var))
+print("String Variable:", string_var, "Type:", type(string_var))
+print("Boolean Variable:", boolean_var, "Type:", type(boolean_var))
